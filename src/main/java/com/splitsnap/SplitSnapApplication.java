fa51a5c3b75992f0f20f38d16b509dfb652ae570
Carlos Huane <carloshuanesarmiento@gmail.com>
2026-05-12 23:19:17 -0500

    feat: inicializar proyecto Spring Boot con dependencias base

---
+++ src/main/java/com/splitsnap/SplitSnapApplication.java
@@ -0,0 +1,12 @@
+package com.splitsnap;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SplitSnapApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SplitSnapApplication.class, args);
+    }
+}
---
+++ src/test/java/com/splitsnap/SplitSnapApplicationTests.java
@@ -0,0 +1,14 @@
+package com.splitsnap;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
+
+@SpringBootTest
+@ActiveProfiles("test")
+class SplitSnapApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+}
